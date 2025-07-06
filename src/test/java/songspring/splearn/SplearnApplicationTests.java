6e6b562da73b9ebefb0b24058009cf6440af0c08
twocowsong <twocowsong@naver.com>
2025-07-06 17:08:16 +0900

    project first setting

---
+++ src/test/java/songspring/splearn/SplearnApplicationTests.java
@@ -0,0 +1,13 @@
+package songspring.splearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

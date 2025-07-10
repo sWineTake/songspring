e407ec2f3ebdbd8a3647b3d96036496c00e11e5b
twocowsong <twocowsong@naver.com>
2025-07-11 06:25:39 +0900

    유저 테스트코드 작성

--- src/main/java/songspring/splearn/domain/Member.java
+++ src/main/java/songspring/splearn/domain/Member.java
@@ -1,34 +1,40 @@
 package songspring.splearn.domain;
 
+import static org.springframework.util.Assert.state;
+import java.util.Objects;
+import lombok.Getter;
+import lombok.ToString;
 
+@Getter
+@ToString
 public class Member {
     private String email;
+
     private String nickname;
+
     private String passwordHash;
+
     private MemberStauts status;
 
     public static Member of(String email, String nickname, String passwordHash) {
         Member member = new Member();
-        member.email = email;
-        member.nickname = nickname;
-        member.passwordHash = passwordHash;
+        member.email = Objects.requireNonNull(email);
+        member.nickname = Objects.requireNonNull(nickname);
+        member.passwordHash = Objects.requireNonNull(passwordHash);
         member.status = MemberStauts.PENDING;
         return member;
     }
 
-    public String getEmail() {
-        return email;
+    public void activate() {
+        state(this.status == MemberStauts.PENDING, "PENDING 상태가 아닙니다");
+
+        this.status = MemberStauts.ACTIVE;
     }
 
-    public String getNickname() {
-        return nickname;
+    public void deactivate() {
+        state(status == MemberStauts.ACTIVE, "PENDING 상태가 아닙니다");
+
+        this.status = MemberStauts.DEACTIVATED;
     }
 
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public MemberStauts getStatus() {
-        return status;
-    }
 }
--- src/test/java/songspring/splearn/domain/MemberTest.java
+++ src/test/java/songspring/splearn/domain/MemberTest.java
@@ -1,6 +1,8 @@
 package songspring.splearn.domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.Test;
 
 class MemberTest {
@@ -9,7 +11,65 @@ class MemberTest {
     void createMember() {
         var member = Member.of("user@naver.com", "User1", "password123");
 
-        Assertions.assertThat(member.getStatus()).isEqualTo(MemberStauts.PENDING);
+        assertThat(member.getStatus()).isEqualTo(MemberStauts.PENDING);
     }
+
+    @Test
+    void 회원_생성자에서_필수값_NULL체크를_하는가() {
+
+        assertThatThrownBy(() -> Member.of(null, "User1", "password"))
+                .isInstanceOf(NullPointerException.class);
+
+    }
+
+    @Test
+    void activate() {
+
+        var member = Member.of("user@naver.com", "User1", "password123");
+
+        member.activate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStauts.ACTIVE);
+
+    }
+
+    @Test
+    void 활성상태_변경_실패() {
+
+        var member = Member.of("user@naver.com", "User1", "password123");
+
+        member.activate();
+
+        assertThatThrownBy(member::activate).isInstanceOf(IllegalStateException.class);
+
+    }
+
+    @Test
+    void 비활성화_상태로_변경할수_있다() {
+
+        var member = Member.of("user@naver.com", "User1", "password123");
+        member.activate();
+
+        member.deactivate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStauts.DEACTIVATED);
+
+    }
+
+    @Test
+    void 비활성_상태에서는_또_비활성_상태로_변경할_수_없다(){
+
+        var member = Member.of("user@naver.com", "User1", "password123");
+
+        assertThatThrownBy(member::deactivate).isInstanceOf(IllegalStateException.class);
+
+        member.activate();
+        member.deactivate();
+
+        assertThatThrownBy(member::deactivate).isInstanceOf(IllegalStateException.class);
+
+    }
+
+
 
 }
